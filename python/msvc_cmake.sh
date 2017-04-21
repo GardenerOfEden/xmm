4914291b2e88e69744e2b0378ae3eea85e6466af
Adam on hppav-14 <chronotopia@gmail.com>
2017-04-21 11:12:33 +0100

    Added shell script for using MSVC to build python bindings (manually passes paths using command line defines to CMake for SWIG and NUMPY include locations)

---
+++ python/msvc_cmake.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cmake -D SWIG_DIR="C:\Program Files (x86)\swigwin-3.0.12" -D SWIG_EXECUTABLE="C:\Program Files (x86)\swigwin-3.0.12\swig.exe" -D NUMPY_INCLUDE="C:\Python\x64\py35\Lib\site-packages\numpy\core\include" . -G"Visual Studio 14 2015 Win64"
